0555c809fdb83b5e5e6c554dadfb183692e321fd
Taffan M <75323709+TaffanM@users.noreply.github.com>
2023-04-14 15:22:56 +0700

    Adding color processing function (color tracking and color picker).

--- Praktikum.py
+++ Praktikum.py
@@ -91,6 +91,10 @@ class ShowImage(QMainWindow):
         self.actionOtsu_Thresholding.triggered.connect(self.thresholdOtsu)
         self.actionContour.triggered.connect(self.contour)
 
+        # Color Processing
+        self.actionColor_Tracking.triggered.connect(self.colorTracking)
+        self.actionColor_Picker.triggered.connect(self.colorPicker)
+
     # Pertemuan 2
 
     #
@@ -916,6 +920,73 @@ class ShowImage(QMainWindow):
             cv2.circle(img, (cx, cy), 2, (0, 255, 0), 2)
 
         cv2.imshow("Contour", img)
+
+    #
+    # Praktek I1
+    #
+
+    def colorTracking(self):
+        cam = cv2.VideoCapture(0)
+        while True:
+            _, frame = cam.read()
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            lower_color = np.array([0, 100, 100])
+            upper_color = np.array([10, 255, 255])
+            mask = cv2.inRange(hsv, lower_color, upper_color)
+            result = cv2.bitwise_and(frame, frame, mask=mask)
+            cv2.imshow("frame", frame)
+            cv2.imshow("mask", mask)
+            cv2.imshow("result", result)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+
+    #
+    # Praktek I2
+    #
+
+    def colorPicker(self):
+        def nothing(x):
+            pass
+
+        cam = cv2.VideoCapture(0)
+        cv2.namedWindow("Trackbars")
+
+        cv2.createTrackbar("L-H", "Trackbars", 0, 179, nothing)
+        cv2.createTrackbar("L-S", "Trackbars", 0, 255, nothing)
+        cv2.createTrackbar("L-V", "Trackbars", 0, 255, nothing)
+        cv2.createTrackbar("U-H", "Trackbars", 179, 179, nothing)
+        cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+        cv2.createTrackbar("U-V", "Trackbars", 255, 255, nothing)
+
+        while True:
+            _, frame = cam.read()
+            hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+            l_h = cv2.getTrackbarPos("L-H", "Trackbars")
+            l_s = cv2.getTrackbarPos("L-S", "Trackbars")
+            l_v = cv2.getTrackbarPos("L-V", "Trackbars")
+            u_h = cv2.getTrackbarPos("U-H", "Trackbars")
+            u_s = cv2.getTrackbarPos("U-S", "Trackbars")
+            u_v = cv2.getTrackbarPos("U-V", "Trackbars")
+
+            lower_color = np.array([l_h, l_s, l_v])
+            upper_color = np.array([u_h, u_s, u_v])
+            mask = cv2.inRange(hsv, lower_color, upper_color)
+            result = cv2.bitwise_and(frame, frame, mask=mask)
+
+            cv2.imshow("frame", frame)
+            cv2.imshow("mask", mask)
+            cv2.imshow("result", result)
+
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+
+        cam.release()
+        cv2.destroyAllWindows()
 
     def displayImage(self, windows=1):
         qformat = QImage.Format_Indexed8
